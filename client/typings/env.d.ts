86aec4adb09ae0c4261194e33f3e21596439023a
Adam Kaminski <adam.kaminski@grupawp.pl>
2021-05-26 10:25:07 +0200

    chore: update env types

--- client/typings/env.d.ts
+++ client/typings/env.d.ts
@@ -1,7 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    REACT_APP_SERVER_ORIGIN: string;
+    REACT_APP_SERVER_PORT: string;
     REACT_APP_TINK_CLIENT_ID: string;
-    REACT_APP_TINK_CLIENT_SECRET: string;
   }
 }
